0b4569977317b4531abf5d07591284250e1b3d06
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-01 23:15:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   new_array.join(" ")
 end
+
+def reverse_each_word(string)
+#  string_array = string.split(" ")
+  new_array = []
+  string.split(" ").collect do |x|
+    new_array.push(x.reverse)
+  end
+  new_array.join(" ")
+end
